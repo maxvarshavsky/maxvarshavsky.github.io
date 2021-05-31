24679f94690f38f75f20989af927eaf4e290be69
maxvarshavsky <shuputush@gmail.com>
2021-05-31 16:47:08 -0700

    Update 03_PlayerCircle.js

--- flappyball/js/03_PlayerCircle.js
+++ flappyball/js/03_PlayerCircle.js
@@ -20,7 +20,7 @@ class PlayerCircle extends GameObject {
   }
 
   render() {
-    let lev = 0;
+    let lev = 1;
     let secs = 0;
     const e = 0;
     if(e == 0) {
@@ -30,20 +30,22 @@ class PlayerCircle extends GameObject {
     if(secs == 60) {
       ++mins
       secs = 0
-    }
+    };
     if (mins == 2) {
       ++lev
       mins = 0
       secs = 0
-    }
+    };
   }
+  if (lev = 1) {
     this.y += this.fallSpeed;
     const {ctx, height} = this.game;
+  };
 
     if (this.y >= height || this.y <= 0) {
       this.game.over();
       return;
-    }
+    };
 
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.width, 0, 2 * Math.PI);
